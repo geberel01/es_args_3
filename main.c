a6c3e28aff046f53c3a1d83073f8ab1ce343934f
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-06 08:14:46 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int i=0;
+
+  printf("numero elementi %d\n",argc);
+  for(i=0;i<argc;i++)
+    printf("n. %d\tval: %s\t\n",i,argv[i]);
+}
